4cd0ef998839cd7c09ed00e92d6a808ff9a57645
Shandaeva Anastassiya <nastya.shandaeva@mail.ru>
2022-11-30 10:55:39 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -40,3 +40,34 @@ function setEnglish() {
     document.getElementById('reset-button').value = 'reset';
 }
 
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function StudentName() {
+    let studentName = document.getElementById('student').innerHTML;
+    alert('Заполняйте данные, ' + studentName + '!!!');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('student').style.backgroundColor = color;
+}
+
+function InfoFooter() {
+    let infoFooter = document.getElementById('footer').innerHTML;
+    alert('Информация о студенте: ' + infoFooter);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.backgroundColor = color;
+}
