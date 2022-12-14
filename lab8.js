c72ff24e9dbc43c168f8bc5f3122439775eac72e
Shandaeva Anastassiya <nastya.shandaeva@mail.ru>
2022-12-14 10:11:01 +0700

    Количество дней с даты рождения + Вывод даты исправленный

--- lab8.js
+++ lab8.js
@@ -4,6 +4,16 @@ function showDate(params) {
     out.innerHTML = today.toLocaleString('ru-Ru');
 }
 
+function showDaysCount(params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil(daysCount);
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;
+}
 
 window.addEventListener('load', showDate);
+
 
