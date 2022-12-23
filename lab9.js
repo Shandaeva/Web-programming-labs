5d3c65c8f1a2298a49800b737538f98af62eab89
Shandaeva Anastassiya <nastya.shandaeva@mail.ru>
2022-12-23 09:42:39 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -11,3 +11,25 @@ function student(button) {
     else
     button.innerHTML = 'Шандаева'
 }
+
+function knopka(button) {
+    if(button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'red';
+    }
+        
+    else if(button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';  
+        button.style.color = 'blue';
+    }
+         
+    else if(button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';   
+        button.style.color = 'pink';
+    }
+        
+    else {
+        button.innerHTML = 'Нажми меня' 
+        button.style.color = 'violet'; 
+    }
+}
